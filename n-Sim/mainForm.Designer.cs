c45114b9cd01c8799e6f902283ae6659cbe19075
Will <will.stephen1995@gmail.com>
2016-04-06 22:09:08 +0100

    Added getDistance() and calcGravity() functions

--- n-Sim/mainForm.Designer.cs
+++ n-Sim/mainForm.Designer.cs
@@ -29,7 +29,10 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainForm));
             this.frameUpdater = new System.Windows.Forms.Timer(this.components);
+            this.xlab = new System.Windows.Forms.Label();
+            this.ylab = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // frameUpdater
@@ -38,23 +41,51 @@
             this.frameUpdater.Interval = 50;
             this.frameUpdater.Tick += new System.EventHandler(this.frameUpdater_Tick);
             // 
+            // xlab
+            // 
+            this.xlab.AutoSize = true;
+            this.xlab.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.xlab.ForeColor = System.Drawing.Color.White;
+            this.xlab.Location = new System.Drawing.Point(13, 13);
+            this.xlab.Name = "xlab";
+            this.xlab.Size = new System.Drawing.Size(28, 18);
+            this.xlab.TabIndex = 0;
+            this.xlab.Text = "x:";
+            // 
+            // ylab
+            // 
+            this.ylab.AutoSize = true;
+            this.ylab.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ylab.ForeColor = System.Drawing.Color.White;
+            this.ylab.Location = new System.Drawing.Point(13, 31);
+            this.ylab.Name = "ylab";
+            this.ylab.Size = new System.Drawing.Size(28, 18);
+            this.ylab.TabIndex = 1;
+            this.ylab.Text = "y:";
+            // 
             // mainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
             this.ClientSize = new System.Drawing.Size(699, 587);
+            this.Controls.Add(this.ylab);
+            this.Controls.Add(this.xlab);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "mainForm";
-            this.Text = "Form1";
+            this.Text = "n-Sim";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.mainForm_Paint);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Timer frameUpdater;
+        private System.Windows.Forms.Label xlab;
+        private System.Windows.Forms.Label ylab;
     }
 }
 
--- n-Sim/mainForm.cs
+++ n-Sim/mainForm.cs
@@ -13,11 +13,13 @@ namespace n_Sim
 {
     public partial class mainForm : Form
     {
+        public readonly double GRAVCONST = 0.001;
+
         Graphics g = null;
         List<Body> bodyList = new List<Body>();
-        Body b1 = new Body(120, 120, 0.5, 0.5, 1000);
-        Body b2 = new Body(239, 120, 2, 1, 2000);
-        
+        Body b1 = new Body(300, 120, 1, 1, 1000);
+        Body b2 = new Body(300, 120, 0, 0, 1000);
+
         public mainForm()
         {
             InitializeComponent();
@@ -27,8 +29,8 @@ namespace n_Sim
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            frameUpdater.Start();
             g = this.CreateGraphics();
+            frameUpdater.Start();
         }
         private void mainForm_Paint(object sender, PaintEventArgs e)
         {
@@ -36,14 +38,31 @@ namespace n_Sim
             {
                 b.drawBody(g);
             }
-            redrawBodies(bodyList);
         }
         
         private void frameUpdater_Tick(object sender, EventArgs e)
         {
-            //redrawBodies(bodyList);
+            
+            redrawBodies(bodyList);
             b1.accelerate(-0.01, -0.01);
-            b2.accelerate(-0.01, -0.01);
+        }
+
+        private double getDistance(Body b1, Body b2)
+        {
+            double distance = 0;
+            double xdiff = Math.Abs(b2.x - b1.x);
+            double ydiff = Math.Abs(b2.y - b1.y);
+            distance = Math.Pow(Math.Pow(xdiff, 2) + Math.Pow(ydiff, 2), 0.5);
+            return distance;
+        }
+
+        private double calcGravity(Body body1, Body body2)
+        {
+            double grav = 0;
+
+            grav = GRAVCONST * ((body1.mass * body2.mass) / Math.Pow((getDistance(body1, body2)), 2));
+
+            return grav;
         }
 
         private void redrawBodies(List<Body> l)
@@ -52,6 +71,7 @@ namespace n_Sim
                 b.updatePosition();
             }
             this.Refresh();
+            Console.WriteLine(calcGravity(b1, b2));
         }
 
     }
